caf1bf4da55a5545b2e254c26711fdb8b225d2f2
Tres Seaver <tseaver@palladion.com>
2013-06-26 14:08:55 -0400

    Update copyright.

--- conf.py
+++ conf.py
@@ -38,7 +38,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'Repoze'
-copyright = u'2010, Agendaless Consulting, Inc. and Contributors'
+copyright = u'2010-2013, Agendaless Consulting, Inc. and Contributors'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
